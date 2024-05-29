6dd68b772f8865cbfe6dd5708418f3ab8f050339
vanhieu272 <93814554+vanhieu272@users.noreply.github.com>
2024-05-30 04:29:18 +0700

    demo data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
+//Interpolation
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   template: `<p>Welcome, {{ username }}!</p>`,
+//   styleUrl: './app.component.css',
+// })
+// export class AppComponent {
+//   username : String = 'Hisu';
+// }
+
+//Propertiy Binding
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet, NgOptimizedImage],
+//   template: `<img [src]="imageUrl" width="200px" height="150px" alt="Logo">`
+// })
+// export class AppComponent {
+//   imageUrl: string = 'https://docs.angularjs.org/img/Two_Way_Data_Binding.png';
+// }
+
+//Event Binding
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   template: `<button (click)="sayHello()">Say Hello</button>`
+// })
+// export class AppComponent {
+//   sayHello() {
+//     alert('Hello!');
+//   }
+// }
+
+//Two-way Binding
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FormsModule],
+  template: `
+    <input [(ngModel)]="username" placeholder="Enter your name">
+    <p>Your name is: {{ username }}</p>
+  `
 })
 export class AppComponent {
-  title = 'DemoDataBinding';
+  username: string = '';
 }
